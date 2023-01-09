c97da0497d562dc01606de51d4582da79a2465b6
alexandre <alexandre.foj@gmail.com>
2023-01-09 09:04:47 -0300

    inserting s3-upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import UploadImageToS3WithReactS3 from './UploadImageToS3WithReactS3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UploadImageToS3WithReactS3 />
     </div>
   );
 }
---
+++ src/UploadImageToS3WithReactS3.js
@@ -0,0 +1,71 @@
+import React , {useState} from 'react';
+import { uploadFile } from 'react-s3';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+window.Buffer = window.Buffer || require("buffer").Buffer; 
+
+const S3_BUCKET ='fada-wine';
+const REGION ='us-east-1';
+const ACCESS_KEY ='AKIAWNDW443R5KVARMMO';
+const SECRET_ACCESS_KEY ='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';
+
+const bucket = new S3Client(
+    {
+        credentials: {
+            accessKeyId: ACCESS_KEY,
+            secretAccessKey: SECRET_ACCESS_KEY,
+        },
+        region: REGION,
+    }
+);
+
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY, 
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
+
+export async function UploadImageToS3(file) {
+    let location = '';
+
+    const params = {
+        Bucket: S3_BUCKET,
+        Key: `wines/` + file.name,
+        Body: file,
+        ACL: 'public-read',
+        ContentType: file.type
+    };
+
+    try {
+        await bucket.send(new PutObjectCommand(params));
+        console.log("SUCCESS");
+        location = 'fada-wine.s3.amazonaws.com/' + `wines/` + file.name;
+    } catch (error) {
+        return error.toString()
+    }
+    
+    return location
+}
+
+const UploadImageToS3WithReactS3 = () => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const handleUpload = async (file) => {
+        uploadFile(file, config)
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }
+
+    return <div>
+        <div>React S3 File Upload</div>
+        <input type="file" onChange={handleFileInput}/>
+        <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+    </div>
+}
+
+export default UploadImageToS3WithReactS3;
